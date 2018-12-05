6f7a7a40cbea4a3876cd906050fd3a352a04d326
asishm <asishkm@gmail.com>
2018-12-05 00:39:56 -0500

    day 5 - bad regex stuff

---
+++ day5.py
@@ -0,0 +1,38 @@
+from itertools import chain, combinations, permutations, zip_longest
+from functools import reduce
+import sys
+import re
+from collections import defaultdict, Counter
+import string
+
+def prob1(line):
+    # print(len(line))
+    while True:
+        line2 = str(line)
+        to_sub1 = r'({})'.format("|".join(f"{l}{l.upper()}" for l in string.ascii_lowercase))
+        to_sub2 = r'({})'.format("|".join(f"{l.upper()}{l}" for l in string.ascii_lowercase))
+        # print(to_sub1)
+        line2 = re.sub(to_sub1, "", line2)
+        line2 = re.sub(to_sub2, "", line2)
+        if len(line2) == len(line):
+            break
+        else:
+            line = line2
+        # print(len(line), len(line2))
+    return len(line)
+
+def prob2(line):
+    min_c = float('inf')
+    for l in string.ascii_lowercase:
+        line2 = line
+        line2 = line2.replace(l, "").replace(l.upper(), "")      
+        out = prob1(line2)
+        print(l, out)
+        min_c = min(min_c, out)
+    return min_c
+
+if __name__ == "__main__":
+    inp = sys.stdin.read().strip()
+
+    print(prob1(inp))
+    print(prob2(inp))
