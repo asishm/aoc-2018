8cec9c5b6c1f6f9ab2543d92ed33821e68a6ea2f
asishm <asishkm@gmail.com>
2018-12-11 06:27:07 -0500

    TIL np.fromfunction - it's really neat

--- day11.py
+++ day11.py
@@ -1,13 +1,23 @@
 from itertools import product
 import numpy as np
-import pandas as pd
+# import pandas as pd
 from numba import jit
 
-def gen_np(xsize, ysize, special_inp, debug=False):
-    grid = pd.DataFrame(list(product(range(1,xsize+1), range(1,ysize+1))), columns=['x', 'y'])
-    grid['power'] = (((grid.x + 10 )*grid.y + special_inp) * (grid.x + 10 ) // 100) % 10 - 5
-    fin = grid.pivot_table(index='x', columns='y').cumsum(axis=1).cumsum()
-    return fin.values
+# def gen_np(xsize, ysize, special_inp, debug=False):
+#     grid = pd.DataFrame(list(product(range(1,xsize+1), range(1,ysize+1))), columns=['x', 'y'])
+#     grid['power'] = (((grid.x + 10 )*grid.y + special_inp) * (grid.x + 10 ) // 100) % 10 - 5
+#     fin = grid.pivot_table(index='x', columns='y').cumsum(axis=1).cumsum()
+#     return fin.values
+
+def power(x, y):
+    rack = x + 10
+    power = rack * y
+    power += 2187
+    power *= rack
+    toret = (power // 100 % 10) - 5
+    toret[0] = 0
+    toret[:,0] = 0
+    return toret.cumsum(axis=0).cumsum(axis=1)
     
 @jit(nopython=True)
 def solve(fin, min_kern, max_kern):
@@ -26,6 +36,6 @@ def solve(fin, min_kern, max_kern):
     return maxv, x+1, y+1, g
 
 if __name__ == "__main__":
-    v1 = gen_np(300, 300, 2187)
+    v1 = np.fromfunction(power, (301, 301))
     print(solve(v1, 3, 4))
     print(solve(v1, 1, 301))
